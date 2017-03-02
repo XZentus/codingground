e7151d61cd03eaf01c806fe723708e2c1f314f13
XZentus <z_xmn@mail.ru>
2017-03-02 08:44:05 +0300

    Tabs->spaces, language
    Changed tabs to spaces, language

--- New Project-20170302/main.lisp
+++ New Project-20170302/main.lisp
@@ -16,30 +16,30 @@
 (defun read-params (ini)
   (with-open-file (params ini :direction :input)
     (setf *from* (trim-return (read-line params nil)))
-	(setf *to*   (trim-return (read-line params nil)))
-	(setf files  (loop for     file = (read-line params nil)
-	                           while   file
-							   collect (trim-return file)))))
+    (setf *to*   (trim-return (read-line params nil)))
+    (setf files  (loop for     file = (read-line params nil)
+                               while   file
+                               collect (trim-return file)))))
 
 (defun main ()
   (read-params +file-ini+)
-  (setf logs (list (format nil "Копирование из ~a в ~a~%" *from* *to*)))
+  (setf logs (list (format nil "Copying из ~a в ~a~%" *from* *to*)))
   (loop for x in files do
     (let ((from (concatenate 'string *from* x))
-	      (to   (concatenate 'string *to*   x)))
+          (to   (concatenate 'string *to*   x)))
       (cond
-	    ((cl-fad:file-exists-p from)
-		  (setf logs (nconc logs (list
-		    (format nil "~a ~a~%" x
-			                      (if (cl-fad:file-exists-p to)
-								      "перезаписан"
-									  "скопирован")))))
-		  (cl-fad:copy-file from to :overwrite t))
-		(t
-		  (setf logs (nconc logs (list (format nil "~a не существует, проигнорирован~%" x))))))))
+        ((cl-fad:file-exists-p from)
+          (setf logs (nconc logs (list
+            (format nil "~a ~a~%" x
+                                  (if (cl-fad:file-exists-p to)
+                                      "rewritten"
+                                      "copied")))))
+          (cl-fad:copy-file from to :overwrite t))
+        (t
+          (setf logs (nconc logs (list (format nil "~a doesn't exist, ignored~%" x))))))))
   (cffi:foreign-funcall "MessageBoxW"
     :pointer (cffi:null-pointer)
-	(:string :encoding :utf-16le) (format nil "~{~A~}" logs)
-	(:string :encoding :utf-16le) "Backup"
-	:int #x40
-	:boolean))
+    (:string :encoding :utf-16le) (format nil "~{~A~}" logs)
+    (:string :encoding :utf-16le) "Backup"
+    :int #x40
+    :boolean))
